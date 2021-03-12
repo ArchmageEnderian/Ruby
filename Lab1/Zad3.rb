532a540875c31ecfefc3f51403da9abc407f257d
EnderIan <iggor.malakhov@gmail.com>
2021-03-12 20:37:49 +0300

    Лаба 1, Задача 3
    Иф - элс

--- Lab1/Zad3.rb
+++ Lab1/Zad3.rb
@@ -1,7 +1,24 @@
 puts "Введите ваш любимый язык программирования: "
-lang = gets.chomp
-if lang == "Ruby" || lang == "ruby" || lang == "RUBY" then
+lang = gets.chomp.downcase
+
+case lang #Использован оператор switch/case
+when "ruby"
 	puts "Не подлизывайся!"
+when "python"
+	puts "Левон, не списывай)"
 else
 	puts "#{lang} не интересно, как Ruby изучишь - сразу станет любимым :)"
+end
+
+puts "1/0/Иное?"
+permach = gets.chomp.to_i
+if permach == 0 #Оператор if/elsif/else
+    puts "#{testVar} = 0"
+
+elsif permach == 1
+    puts "#{testVar} = 1"
+
+else
+    puts "#{testVar} = ачтогде"
+
 end
