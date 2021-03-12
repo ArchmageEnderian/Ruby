cfc95b20a1488db8d065f979e1be59d2bb911e2e
EnderIan <iggor.malakhov@gmail.com>
2021-03-12 20:45:43 +0300

    Лаба 1, Задача 3
    Добавлен unless

--- Lab1/Zad3.rb
+++ Lab1/Zad3.rb
@@ -13,12 +13,20 @@ end
 puts "1/0/Иное?"
 permach = gets.chomp.to_i
 if permach == 0 #Оператор if/elsif/else
-    puts "#{testVar} = 0"
+    puts "#{permach} = 0 (if)"
 
 elsif permach == 1
-    puts "#{testVar} = 1"
+    puts "#{permach} = 1 (elsif)"
 
 else
-    puts "#{testVar} = ачтогде"
+    puts "#{permach} = ачтогде (classic else)"
+
+end
+
+unless permach == 0 #Оператор unless
+    puts "#{permach} = 0 (unless)"
+
+else
+    puts "#{permach} = 0 (else(unless))"
 
 end
