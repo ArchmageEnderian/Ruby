b3ab17e4e31d37bcf636a8cc2288adf24ab9927b
EnderIan <iggor.malakhov@gmail.com>
2021-03-13 04:33:25 +0300

    Лаба 2, Задача 9
    конец

--- Lab2/Zad9.rb
+++ Lab2/Zad9.rb
@@ -11,6 +11,20 @@ def Num2(str)
 	}
 end
 
+def Num3(str)
+	l = str.length
+	str1 = []
+	str.map { |n| 
+		if n.strip.downcase == "белый"
+			str1.unshift(n)
+		elsif n.strip.downcase == "красный"
+			str1 << n
+		else
+			str1.insert(l/2, n)
+		end
+	}
+	return str1.compact.join(" ")
+end
 
 puts "Введите строку: "
 main_str = gets.chomp.split()
@@ -24,5 +38,5 @@ when "1"
 when "2"
 	Num2(main_str.dup)
 when "3"
-	#print "Ответ: ", Num3?(array), "\n"
+	print "Ответ: ", Num3(main_str), "\n"
 end
